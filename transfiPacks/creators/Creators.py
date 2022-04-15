93ff6a7e219ae320a3b53780f7f2e720d282a3eb
RahulDubey391 <100185371+RahulDubey391@users.noreply.github.com>
2022-04-15 16:14:23 +0530

    Update Creators.py

--- transfiPacks/creators/Creators.py
+++ transfiPacks/creators/Creators.py
@@ -5,8 +5,17 @@ class BigQueryCreator(BigQueryConfig):
         super().__init__(configDict)
         self.con = self.get_con()
     
+    def create_schema(self):
+        if self.fields !=None:
+            self.target_schema = {}
+            for i in self.fields:
+                self.target_schema[i] = 'String'
+        return self.target_schema
+
+    
     def create_table(self):
-        create_query = f"""CREATE OR REPLACE TABLE %s.%s.%s"""%(self.project_id,self.database_name,self.table_name)
+        target_schema = self.create_schema()
+        create_query = f"""CREATE OR REPLACE TABLE %s.%s.%s (%s)"""%(self.project_id,self.database_name,self.table_name,target_schema)
         self.con.query(create_query)
 
 class SnowfalkeCreator(SnowflakeConfig):
@@ -14,8 +23,16 @@ class SnowfalkeCreator(SnowflakeConfig):
         super().__init__(configDict)
         self.con = self.get_con()
     
+    def create_schema(self):
+        if self.fields !=None:
+            self.target_schema = {}
+            for i in self.fields:
+                self.target_schema[i] = 'String'
+        return self.target_schema
+
     def create_table(self):
-        create_query = f"""CREATE OR REPALCE TABLE %s.%s.%s"""%(self.database_name,self.schema_name,self.table_name)
+        target_schema = self.create_schema()
+        create_query = f"""CREATE OR REPALCE TABLE %s.%s.%s (%s)"""%(self.database_name,self.schema_name,self.table_name,target_schema)
         self.con.cursor().execute(create_query)
 
 class SalesforceCreator(SalesForceConfig):
@@ -23,5 +40,12 @@ class SalesforceCreator(SalesForceConfig):
         super().__init__(configDict)
         self.con = self.get_con()
     
+    def create_schema(self):
+        if self.fields !=None:
+            self.target_schema = {}
+            for i in self.fields:
+                self.target_schema[i] = 'String'
+        return self.target_schema
+
     def create_table(self):
         return 'Done'
